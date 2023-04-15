732f19c5d46b346df93ce06a05fbf99d6d404148
shinyj1991 <63823237+shinyj1991@users.noreply.github.com>
2023-04-15 00:11:47 +0000

    Deploying to gh-pages from @ shinyj1991/blog.simplizm.xyz@ac870db5f2c883bb536de1f490780d96e9657652 🚀

--- pages/about.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="page-about">
-    about
-
-    <div>
-      <nuxt-link to="/">go to home</nuxt-link>
-    </div>
-  </div>
-</template>
--- pages/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>index</div>
-  <div>
-    <nuxt-link to="/about">go to about</nuxt-link>
-  </div>
-  </template>
